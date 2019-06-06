f3e413938378ba29f4a9a115d147eb4d0751a6b3
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2019-06-06 14:54:51 +0200

    added findLyrics and playSong usecase

---
+++ domain/src/main/java/com/chase/kudzie/chasemusic/domain/interactor/songs/PlaySong.kt
@@ -0,0 +1,8 @@
+package com.chase.kudzie.chasemusic.domain.interactor.songs
+
+class PlaySong {
+    //The implementation of this is tricky
+
+    // --> Must allow retrieving song details
+    // --> Must allow retrieving some trivial details like lyrics
+}
