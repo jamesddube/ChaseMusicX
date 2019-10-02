310cf55cf8a7ef5145b2672863420b22b128a3fc
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2019-10-02 16:55:51 +0200

    created ArtistLoader and SongLoader classes to assist with data loading

---
+++ data/src/main/java/com/chase/kudzie/chasemusic/data/loaders/ArtistLoader.kt
@@ -0,0 +1,83 @@
+package com.chase.kudzie.chasemusic.data.loaders
+
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.database.Cursor
+import android.net.Uri
+import android.provider.BaseColumns._ID
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.ArtistColumns.*
+import android.provider.MediaStore.Audio.AudioColumns
+import com.chase.kudzie.chasemusic.data.sorting.SortOrder
+
+internal class ArtistLoader(
+    private val contentResolver: ContentResolver
+) {
+
+    companion object {
+        val uri: Uri = MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(
+            _ID,
+            ARTIST,
+            NUMBER_OF_ALBUMS,
+            NUMBER_OF_TRACKS
+        )
+
+        val sortOrder = SortOrder.ArtistSortOrder.ARTIST_A_Z
+    }
+
+    @SuppressLint("Recycle")
+    fun getAll(): Cursor {
+        val selection = null
+        val selectionArgs = null
+
+        return contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)!!
+    }
+
+    @SuppressLint("Recycle")
+    fun getArtist(id: Long): Cursor {
+        val selection = "$_ID=?"
+        val selectionArgs = arrayOf(
+            "$id"
+        )
+
+        return contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)!!
+    }
+
+    @SuppressLint("Recycle")
+    fun findArtists(searchString: String): Cursor {
+        val selection = "$ARTIST LIKE?"
+        val selectionArgs = arrayOf(
+            "%$searchString%"
+        )
+
+        return contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)!!
+    }
+
+
+    @SuppressLint("Recycle")
+    fun getArtistAlbums(id: Long): Cursor {
+        val uri = MediaStore.Audio.Artists.Albums.getContentUri("external", id)
+        val projection = AlbumLoader.projection
+        val sortOrder = AlbumLoader.sortOrder
+        val selection = null
+        val selectionArgs = null
+
+        return contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)!!
+    }
+
+    @SuppressLint("Recycle")
+    fun getArtistSongs(id: Long): Cursor {
+        val selection =
+            "${AudioColumns.IS_MUSIC} = 1 " +
+                    "AND ${AudioColumns.TITLE} != '' " +
+                    "AND ${AudioColumns.ARTIST_ID}=$id"
+        val selectionArgs = null
+        val uri = SongLoader.uri
+        val projection = SongLoader.projection
+        val sortOrder = SongLoader.sortOrder
+
+        return contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)!!
+    }
+}
---
+++ data/src/main/java/com/chase/kudzie/chasemusic/data/loaders/SongLoader.kt
@@ -0,0 +1,63 @@
+package com.chase.kudzie.chasemusic.data.loaders
+
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.database.Cursor
+import android.net.Uri
+import android.provider.BaseColumns
+import android.provider.BaseColumns._ID
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.AudioColumns.*
+import android.provider.MediaStore.MediaColumns.DURATION
+import android.provider.MediaStore.MediaColumns.TITLE
+import com.chase.kudzie.chasemusic.data.sorting.SortOrder
+
+internal class SongLoader(
+    private val contentResolver: ContentResolver
+) {
+
+    companion object {
+        val uri: Uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(
+            _ID,
+            TITLE,
+            ARTIST,
+            ALBUM,
+            DURATION,
+            TRACK,
+            ARTIST_ID,
+            ALBUM_ID,
+            IS_MUSIC
+        )
+        val sortOrder = SortOrder.SongSortOrder.SONG_A_Z
+    }
+
+    @SuppressLint("Recycle")
+    fun getAll(): Cursor {
+        val selection = null
+        val selectionArgs = null
+
+        return contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)!!
+    }
+
+    @SuppressLint("Recycle")
+    fun getSong(id: Long): Cursor {
+        val selection = "$_ID=?"
+        val selectionArgs = arrayOf(
+            "$id"
+        )
+
+        return contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)!!
+    }
+
+
+    @SuppressLint("Recycle")
+    fun findSongs(searchString: String): Cursor {
+        val selection = "$TITLE LIKE ?"
+        val selectionArgs = arrayOf(
+            "%$searchString%"
+        )
+
+        return contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)!!
+    }
+}
