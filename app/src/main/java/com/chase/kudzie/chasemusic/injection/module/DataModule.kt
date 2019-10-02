0472566df7053d2c096482ec58f6a47cb5eb078b
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2019-10-02 16:57:29 +0200

    injection

--- app/src/main/java/com/chase/kudzie/chasemusic/injection/module/DataModule.kt
+++ app/src/main/java/com/chase/kudzie/chasemusic/injection/module/DataModule.kt
@@ -1,8 +1,13 @@
 package com.chase.kudzie.chasemusic.injection.module
 
-
 import com.chase.kudzie.chasemusic.data.repository.AlbumRepositoryImpl
+import com.chase.kudzie.chasemusic.data.repository.ArtistRepositoryImpl
+import com.chase.kudzie.chasemusic.data.repository.PlaylistRepositoryImpl
+import com.chase.kudzie.chasemusic.data.repository.SongRepositoryImpl
 import com.chase.kudzie.chasemusic.domain.repository.AlbumRepository
+import com.chase.kudzie.chasemusic.domain.repository.ArtistRepository
+import com.chase.kudzie.chasemusic.domain.repository.PlaylistRepository
+import com.chase.kudzie.chasemusic.domain.repository.SongRepository
 import dagger.Binds
 import dagger.Module
 
@@ -11,4 +16,13 @@ abstract class DataModule {
 
     @Binds
     abstract fun bindsAlbumsRepository(albumsRepository: AlbumRepositoryImpl): AlbumRepository
+
+    @Binds
+    abstract fun bindsArtistsRepository(artistRepository: ArtistRepositoryImpl): ArtistRepository
+
+    @Binds
+    abstract fun bindsSongRepository(songRepository: SongRepositoryImpl): SongRepository
+
+    @Binds
+    abstract fun bindsPlaylistRepository(playlistRepository: PlaylistRepositoryImpl): PlaylistRepository
 }
