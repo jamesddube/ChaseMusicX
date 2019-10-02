ae0e0eab67cf62e22ccfb2372fd638472e293fdc
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2019-10-02 16:58:00 +0200

    created PlaylistLoader class

---
+++ data/src/main/java/com/chase/kudzie/chasemusic/data/loaders/PlaylistLoader.kt
@@ -0,0 +1,8 @@
+package com.chase.kudzie.chasemusic.data.loaders
+
+import android.content.ContentResolver
+
+internal class PlaylistLoader(
+    private val contentResolver: ContentResolver
+) {
+}
