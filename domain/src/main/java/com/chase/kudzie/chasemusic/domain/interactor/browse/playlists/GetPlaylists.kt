99f8b2baeab3a0b6ff8fb4095eeba3bc95b3859e
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2019-10-31 12:57:31 +0200

    playlist repository

--- domain/src/main/java/com/chase/kudzie/chasemusic/domain/interactor/browse/playlists/GetPlaylists.kt
+++ domain/src/main/java/com/chase/kudzie/chasemusic/domain/interactor/browse/playlists/GetPlaylists.kt
@@ -1,7 +1,13 @@
 package com.chase.kudzie.chasemusic.domain.interactor.browse.playlists
 
+import com.chase.kudzie.chasemusic.domain.repository.PlaylistRepository
+import javax.inject.Inject
+
 /**
  * @author Kudzai Chasinda
  */
-class GetPlaylists {
+class GetPlaylists @Inject constructor(
+    val repository: PlaylistRepository
+) {
+    suspend operator fun invoke() = repository.getPlaylists()
 }
--- domain/src/main/java/com/chase/kudzie/chasemusic/domain/repository/PlaylistRepository.kt
+++ domain/src/main/java/com/chase/kudzie/chasemusic/domain/repository/PlaylistRepository.kt
@@ -1,6 +1,10 @@
 package com.chase.kudzie.chasemusic.domain.repository
 
+import com.chase.kudzie.chasemusic.domain.model.Playlist
+
 interface PlaylistRepository {
+
+    suspend fun getPlaylists(): List<Playlist>
 
     suspend fun makePlaylist()
 
