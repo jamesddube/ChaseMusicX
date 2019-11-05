8132da2054815cb91f74b21092a56885f4ed4eee
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2019-11-05 15:47:44 +0200

    remove dummy test

--- app/src/main/java/com/chase/kudzie/chasemusic/MainActivity.kt
+++ app/src/main/java/com/chase/kudzie/chasemusic/MainActivity.kt
@@ -14,28 +14,9 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var viewModelFactory: ViewModelFactory
-
-    lateinit var viewModel: AlbumViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initViewModels()
-
-        //Quick test
-        viewModel.albums.observe(this, Observer {
-            it?.let {
-                Timber.e("Size of Albums:: ${it.size}")
-                Toast.makeText(this, "Size of Albums:: ${it.size}", Toast.LENGTH_LONG).show()
-            }
-        })
-    }
-
-    private fun initViewModels() {
-        viewModel = ViewModelProviders.of(this, viewModelFactory).get(AlbumViewModel::class.java)
     }
 }
