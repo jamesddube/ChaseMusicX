12a4028bdfbd48ca9b5db36067f1f4d3110b8d54
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2020-01-01 21:00:08 +0200

    created shared module to handle shared instances

---
+++ shared/src/main/java/com/kudziechase/chasemusic/shared/Extensions.kt
@@ -0,0 +1,2 @@
+package com.kudziechase.chasemusic.shared
+
--- shared/src/main/java/com/kudziechase/chasemusic/shared/injection/module/DataModule.kt
+++ shared/src/main/java/com/kudziechase/chasemusic/shared/injection/module/DataModule.kt
@@ -1,4 +1,4 @@
-package com.chase.kudzie.chasemusic.injection.module
+package com.kudziechase.chasemusic.shared.injection.module
 
 import com.chase.kudzie.chasemusic.data.repository.AlbumRepositoryImpl
 import com.chase.kudzie.chasemusic.data.repository.ArtistRepositoryImpl
@@ -10,19 +10,24 @@ import com.chase.kudzie.chasemusic.domain.repository.PlaylistRepository
 import com.chase.kudzie.chasemusic.domain.repository.SongRepository
 import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 abstract class DataModule {
 
     @Binds
+    @Singleton
     abstract fun bindsAlbumsRepository(albumsRepository: AlbumRepositoryImpl): AlbumRepository
 
     @Binds
+    @Singleton
     abstract fun bindsArtistsRepository(artistRepository: ArtistRepositoryImpl): ArtistRepository
 
     @Binds
+    @Singleton
     abstract fun bindsSongRepository(songRepository: SongRepositoryImpl): SongRepository
 
     @Binds
+    @Singleton
     abstract fun bindsPlaylistRepository(playlistRepository: PlaylistRepositoryImpl): PlaylistRepository
 }
