1b0976e6e4fb00b5a4d22b47f514fffbe5adc8e7
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2020-07-09 13:26:09 +0200

    add queue entity and queue dao

--- data/src/main/java/com/chase/kudzie/chasemusic/data/database/entities/QueueSongEntity.kt
+++ data/src/main/java/com/chase/kudzie/chasemusic/data/database/entities/QueueSongEntity.kt
@@ -1,4 +1,16 @@
 package com.chase.kudzie.chasemusic.data.database.entities
 
-class SongEntity {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * This class represents a song in a queue
+ * */
+
+@Entity(tableName = "queue")
+data class QueueSongEntity(
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "song_id") var songId: Long,
+    @ColumnInfo(name = "queue_position") var positionInQueue: Int
+)
