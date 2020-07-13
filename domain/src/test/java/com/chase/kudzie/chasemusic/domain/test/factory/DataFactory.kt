8b3a509909bc0e6594116882b158d33a7093e9d4
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2020-07-13 16:54:49 +0200

    add unit tests
    test GetSongs Use Case
    
    test GetAlbums use case
    
    test GetArtists use case

--- domain/src/test/java/com/chase/kudzie/chasemusic/domain/test/factory/DataFactory.kt
+++ domain/src/test/java/com/chase/kudzie/chasemusic/domain/test/factory/DataFactory.kt
@@ -2,23 +2,28 @@ package com.chase.kudzie.chasemusic.domain.test.factory
 
 import kotlin.random.Random
 
-
 /**
  * Factory class for data instances
  */
-class DataFactory {
-    companion object Factory {
-
-        fun randomUuid(): String {
-            return java.util.UUID.randomUUID().toString()
-        }
-
-        fun randomInt(): Int {
-            return Random(216327).nextInt()
-        }
-
-        fun randomLong(): Long {
-            return Random(342L).nextLong()
-        }
+object DataFactory {
+    fun randomUuid(): String {
+        return java.util.UUID.randomUUID().toString()
     }
+
+    fun randomString(): String {
+        val source = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz"
+        return java.util.Random().ints(10, 0, source.length)
+            .toArray()
+            .map(source::get)
+            .joinToString("")
+    }
+
+    fun randomInt(): Int {
+        return Random(216327).nextInt()
+    }
+
+    fun randomLong(): Long {
+        return Random(342L).nextLong()
+    }
+
 }
