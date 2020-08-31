22b6d34b767d0322bb8388f9cb63a698a19bbc0a
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2020-08-31 10:35:24 +0200

    refactor
    added const to sort order class

--- data/src/main/java/com/chase/kudzie/chasemusic/data/sorting/SortOrder.kt
+++ data/src/main/java/com/chase/kudzie/chasemusic/data/sorting/SortOrder.kt
@@ -6,16 +6,16 @@ class SortOrder {
     interface ArtistSortOrder {
         companion object {
             /* Artist sort order A-Z */
-            val ARTIST_A_Z = MediaStore.Audio.Artists.DEFAULT_SORT_ORDER
+            const val ARTIST_A_Z = MediaStore.Audio.Artists.DEFAULT_SORT_ORDER
 
             /* Artist sort order Z-A */
-            val ARTIST_Z_A = "$ARTIST_A_Z DESC"
+            const val ARTIST_Z_A = "$ARTIST_A_Z DESC"
 
             /* Artist sort order number of songs */
-            val ARTIST_NUMBER_OF_SONGS = MediaStore.Audio.Artists.NUMBER_OF_TRACKS + " DESC"
+            const val ARTIST_NUMBER_OF_SONGS = MediaStore.Audio.Artists.NUMBER_OF_TRACKS + " DESC"
 
             /* Artist sort order number of albums */
-            val ARTIST_NUMBER_OF_ALBUMS = MediaStore.Audio.Artists.NUMBER_OF_ALBUMS + " DESC"
+            const val ARTIST_NUMBER_OF_ALBUMS = MediaStore.Audio.Artists.NUMBER_OF_ALBUMS + " DESC"
         }
     }
 
@@ -25,19 +25,19 @@ class SortOrder {
     interface AlbumSortOrder {
         companion object {
             /* Album sort order A-Z */
-            val ALBUM_A_Z = MediaStore.Audio.Albums.DEFAULT_SORT_ORDER
+            const val ALBUM_A_Z = MediaStore.Audio.Albums.DEFAULT_SORT_ORDER
 
             /* Album sort order Z-A */
-            val ALBUM_Z_A = "$ALBUM_A_Z DESC"
+            const val ALBUM_Z_A = "$ALBUM_A_Z DESC"
 
             /* Album sort order songs */
-            val ALBUM_NUMBER_OF_SONGS = MediaStore.Audio.Albums.NUMBER_OF_SONGS + " DESC"
+            const val ALBUM_NUMBER_OF_SONGS = MediaStore.Audio.Albums.NUMBER_OF_SONGS + " DESC"
 
             /* Album sort order artist */
-            val ALBUM_ARTIST = MediaStore.Audio.Albums.ARTIST
+            const val ALBUM_ARTIST = MediaStore.Audio.Albums.ARTIST
 
             /* Album sort order year */
-            val ALBUM_YEAR = MediaStore.Audio.Albums.FIRST_YEAR + " DESC"
+            const val ALBUM_YEAR = MediaStore.Audio.Albums.FIRST_YEAR + " DESC"
         }
 
     }
@@ -48,28 +48,28 @@ class SortOrder {
     interface SongSortOrder {
         companion object {
             /* Song sort order A-Z */
-            val SONG_A_Z = MediaStore.Audio.Media.DEFAULT_SORT_ORDER
+            const val SONG_A_Z = MediaStore.Audio.Media.DEFAULT_SORT_ORDER
 
             /* Song sort order Z-A */
-            val SONG_Z_A = "$SONG_A_Z DESC"
+            const val SONG_Z_A = "$SONG_A_Z DESC"
 
             /* Song sort order artist */
-            val SONG_ARTIST = MediaStore.Audio.Media.ARTIST
+            const val SONG_ARTIST = MediaStore.Audio.Media.ARTIST
 
             /* Song sort order album */
-            val SONG_ALBUM = MediaStore.Audio.Media.ALBUM
+            const val SONG_ALBUM = MediaStore.Audio.Media.ALBUM
 
             /* Song sort order year */
-            val SONG_YEAR = MediaStore.Audio.Media.YEAR + " DESC"
+            const val SONG_YEAR = MediaStore.Audio.Media.YEAR + " DESC"
 
             /* Song sort order duration */
-            val SONG_DURATION = MediaStore.Audio.Media.DURATION + " DESC"
+            const val SONG_DURATION = MediaStore.Audio.Media.DURATION + " DESC"
 
             /* Song sort order date */
-            val SONG_DATE = MediaStore.Audio.Media.DATE_ADDED + " DESC"
+            const val SONG_DATE = MediaStore.Audio.Media.DATE_ADDED + " DESC"
 
             /* Song sort order filename */
-            val SONG_FILENAME = MediaStore.Audio.Media.DATA
+            const val SONG_FILENAME = MediaStore.Audio.Media.DATA
         }
     }
 
@@ -79,23 +79,23 @@ class SortOrder {
     interface AlbumSongSortOrder {
         companion object {
             /* Album song sort order A-Z */
-            val SONG_A_Z = MediaStore.Audio.Media.DEFAULT_SORT_ORDER
+            const val SONG_A_Z = MediaStore.Audio.Media.DEFAULT_SORT_ORDER
 
             /* Album song sort order Z-A */
-            val SONG_Z_A = "$SONG_A_Z DESC"
+            const val SONG_Z_A = "$SONG_A_Z DESC"
 
             /* Album song sort order track list */
-            val SONG_TRACK_LIST = (MediaStore.Audio.Media.TRACK + ", "
+            const val SONG_TRACK_LIST = (MediaStore.Audio.Media.TRACK + ", "
                     + MediaStore.Audio.Media.DEFAULT_SORT_ORDER)
 
             /* Album song sort order duration */
-            val SONG_DURATION = SongSortOrder.SONG_DURATION
+            const val SONG_DURATION = SongSortOrder.SONG_DURATION
 
             /* Album Song sort order year */
-            val SONG_YEAR = MediaStore.Audio.Media.YEAR + " DESC"
+            const val SONG_YEAR = MediaStore.Audio.Media.YEAR + " DESC"
 
             /* Album song sort order filename */
-            val SONG_FILENAME = SongSortOrder.SONG_FILENAME
+            const val SONG_FILENAME = SongSortOrder.SONG_FILENAME
         }
     }
 
@@ -105,25 +105,25 @@ class SortOrder {
     interface ArtistSongSortOrder {
         companion object {
             /* Artist song sort order A-Z */
-            val SONG_A_Z = MediaStore.Audio.Media.DEFAULT_SORT_ORDER
+            const val SONG_A_Z = MediaStore.Audio.Media.DEFAULT_SORT_ORDER
 
             /* Artist song sort order Z-A */
-            val SONG_Z_A = "$SONG_A_Z DESC"
+            const val SONG_Z_A = "$SONG_A_Z DESC"
 
             /* Artist song sort order album */
-            val SONG_ALBUM = MediaStore.Audio.Media.ALBUM
+            const val SONG_ALBUM = MediaStore.Audio.Media.ALBUM
 
             /* Artist song sort order year */
-            val SONG_YEAR = MediaStore.Audio.Media.YEAR + " DESC"
+            const val SONG_YEAR = MediaStore.Audio.Media.YEAR + " DESC"
 
             /* Artist song sort order duration */
-            val SONG_DURATION = MediaStore.Audio.Media.DURATION + " DESC"
+            const val SONG_DURATION = MediaStore.Audio.Media.DURATION + " DESC"
 
             /* Artist song sort order date */
-            val SONG_DATE = MediaStore.Audio.Media.DATE_ADDED + " DESC"
+            const val SONG_DATE = MediaStore.Audio.Media.DATE_ADDED + " DESC"
 
             /* Artist song sort order filename */
-            val SONG_FILENAME = SongSortOrder.SONG_FILENAME
+            const val SONG_FILENAME = SongSortOrder.SONG_FILENAME
         }
     }
 
@@ -133,16 +133,16 @@ class SortOrder {
     interface ArtistAlbumSortOrder {
         companion object {
             /* Artist album sort order A-Z */
-            val ALBUM_A_Z = MediaStore.Audio.Albums.DEFAULT_SORT_ORDER
+            const val ALBUM_A_Z = MediaStore.Audio.Albums.DEFAULT_SORT_ORDER
 
             /* Artist album sort order Z-A */
-            val ALBUM_Z_A = "$ALBUM_A_Z DESC"
+            const val ALBUM_Z_A = "$ALBUM_A_Z DESC"
 
             /* Artist album sort order songs */
-            val ALBUM_NUMBER_OF_SONGS = MediaStore.Audio.Artists.Albums.NUMBER_OF_SONGS + " DESC"
+            const val ALBUM_NUMBER_OF_SONGS = MediaStore.Audio.Artists.Albums.NUMBER_OF_SONGS + " DESC"
 
             /* Artist album sort order year */
-            val ALBUM_YEAR = MediaStore.Audio.Artists.Albums.FIRST_YEAR + " DESC"
+            const  val ALBUM_YEAR = MediaStore.Audio.Artists.Albums.FIRST_YEAR + " DESC"
         }
     }
 
