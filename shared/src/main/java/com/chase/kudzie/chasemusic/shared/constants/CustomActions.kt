6ac063c3b92e757130cacaa324e36ee4fc32535d
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2020-12-03 18:00:19 +0200

    custom actions
    define custom action strings

---
+++ shared/src/main/java/com/chase/kudzie/chasemusic/shared/constants/CustomActions.kt
@@ -0,0 +1,9 @@
+package com.chase.kudzie.chasemusic.shared.constants
+
+object CustomActions {
+    const val SWAP = "action_swap"
+    const val SHUFFLE_PLAY = "action_shuffle_play"
+    const val ADD_TO_QUEUE = "action_add_to_queue"
+    const val REMOVE_FROM_QUEUE = "action_remove_from_queue"
+    const val PLAY_NEXT = "action_play_next"
+}
